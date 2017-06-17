4fe234855a6eb80bd13ec528b16303ddbba8d18e
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 11:02:05 +0200

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MdToolbarModule,
     AppRoutingModule
   ],
   providers: [],
