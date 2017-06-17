62472beec09b06e5d3304ec284f70701fdedd86b
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 13:16:34 +0200

    Add create task dialog opening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdIconModule, MdListModule, MdToolbarModule } from '@angular/material';
+import {
+  MdButtonModule,
+  MdDialogModule,
+  MdIconModule,
+  MdListModule,
+  MdToolbarModule
+} from '@angular/material';
 import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,18 +16,22 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskService } from './task.service';
 import { TaskListItemComponent } from './task-list-item/task-list-item.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    TaskListItemComponent
+    TaskListItemComponent,
+    EditTaskComponent
   ],
+  entryComponents: [EditTaskComponent],
   imports: [
     BrowserModule,
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule,
+    MdDialogModule,
     MdIconModule,
     MdListModule,
     MdToolbarModule,
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 
 import { TaskService } from '../task.service';
 import { Task } from '../task';
+import { EditTaskComponent } from '../edit-task/edit-task.component';
 
 @Component({
   selector: 'app-task-list',
@@ -12,14 +14,18 @@ import { Task } from '../task';
 export class TaskListComponent implements OnInit {
   tasks: Observable<Task[]>;
 
-  constructor(private taskService: TaskService) {
+  constructor(private taskService: TaskService, private dialog: MdDialog) {
   }
 
   ngOnInit() {
     this.tasks = this.taskService.find();
   }
 
-  addTask() {
-    
+  openCreateTaskDialog() {
+    const dialogRef = this.dialog.open(EditTaskComponent);
+    const closedSub = dialogRef.afterClosed().subscribe(result => {
+      console.info('Dialog closed', result);
+      closedSub.unsubscribe();
+    });
   }
 }
