c61171fd15c7d23ca6e07b5f85a745d58d146457
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 11:06:53 +0200

    Add the task service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskService } from './task.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { TaskListComponent } from './task-list/task-list.component';
     MdToolbarModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
