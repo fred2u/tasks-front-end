0e1cf5c19df8dba8bc9d0a4c46e90646a885af9e
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 11:27:17 +0200

    Fix missing http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdToolbarModule } from '@angular/material';
 import 'hammerjs';
@@ -16,6 +17,7 @@ import { TaskService } from './task.service';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     BrowserAnimationsModule,
     MdToolbarModule,
     AppRoutingModule
