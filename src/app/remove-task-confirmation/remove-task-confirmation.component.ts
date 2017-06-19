2c3e607b7f3ee53616245e874e0de33fd4c8e681
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-19 18:09:48 +0200

    Add remove task dialog content

--- src/app/remove-task-confirmation/remove-task-confirmation.component.ts
+++ src/app/remove-task-confirmation/remove-task-confirmation.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+
+import { Task } from '../task';
 
 @Component({
   selector: 'app-remove-task-confirmation',
   templateUrl: './remove-task-confirmation.component.html',
   styleUrls: ['./remove-task-confirmation.component.scss']
 })
-export class RemoveTaskConfirmationComponent implements OnInit {
+export class RemoveTaskConfirmationComponent {
+  task: Task;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private dialog: MdDialogRef<RemoveTaskConfirmationComponent>, @Inject(MD_DIALOG_DATA) data: any) {
+    this.task = data.task;
   }
-
 }
