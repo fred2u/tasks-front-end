9b762067f970775ec2df8958b6a0556c9bc39444
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-19 08:06:50 +0200

    Add express

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+const www = path.join(__dirname, 'dist');
+const app = express();
+
+// Middlewares
+app.use(compression());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.static(www));
+app.use('*', (req, res) => res.sendFile('index.html', { root: www }));
+app.use((err, req, res, next) => res.status(err.status || 500).send(err));
+
+module.exports = app;
