cca9b2333dc8280c5fcee995bc6b1212e54efbd5
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-07 08:59:39 +0100

    Update to angular-cli 1.5 and Angular 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { TasksFrontEndPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('tasks-front-end App', () => {
-  let page: TasksFrontEndPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new TasksFrontEndPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
